5199bd075c1c4d78ceddc6f88e4a68a393a602c4
PNNutkung <pipatpol.t@ku.th>
2016-07-23 16:39:42 +0700

    change jquery to react (not pure react)

--- components/wrapper.jsx
+++ components/wrapper.jsx
@@ -1,20 +1,49 @@
-var React = require("react");
+//var React = require("react");
 var ReactDOM = require("react-dom");
 
 (function() {
-  'use strict';
+    'use strict';
 
     module.exports = React.createClass({
-      render: function() {
-        return (
-          <div>
-            <h1>Hello World</h1>
-          </div>
-        );
-      }
+        getInitialState: function() {
+            return {
+                a: 0,
+                b: 0
+            }
+        },
+        add: function() {
+            var aInput = $('#a');
+            var bInput = $('#b');
+            var result = $('#result');
+            var r = parseFloat(aInput.val()) + parseFloat(bInput.val());
+            result.html(r);
+        },
+        sub: function() {
+            var aInput = $('#a');
+            var bInput = $('#b');
+            var result = $('#result');
+            var r = parseFloat(aInput.val()) - parseFloat(bInput.val());
+            result.html(r);
+        },
+        render: function() {
+            return (
+                <div id="container">
+                    <h1 id="result">0</h1>
+                    <div>
+                        <input id="a"/>
+                        <input id="b"/>
+                    </div>
+                    <div>
+                        <button onClick={this.add}>ADD</button>
+                        <button onClick={this.sub}>SUB</button>
+                    </div>
+                </div>
+            );
+        }
     });
 
 }());
 
 var Wrapper = require('./wrapper.jsx');
-ReactDOM.render(<Wrapper/>, document.getElementById("container"));
+ReactDOM.render(
+    <Wrapper/>, document.getElementById("container"));
